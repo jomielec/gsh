127fb1bdf265e6733477eff64a7c325ae6e16c58
Joshua Focht <joshua.focht@outlook.com>
2024-11-28 14:09:24 -0500

    rename directories to files

--- src/ls.zig
+++ src/ls.zig
@@ -4,7 +4,7 @@
 
 // pub fn main() !void {
 //     const cwd = std.fs.cwd(); // Get the current working directory
-//     var directories = ArrayList([]const u8).init(std.heap.page_allocator);
+//     var files = ArrayList([]const u8).init(std.heap.page_allocator);
 
 //     // Open the current directory explicitly with "."
 //     var dir = try cwd.openDir("./", .{});
@@ -17,21 +17,17 @@
 //         const entry = try iterator.next();
 //         if (entry == null) break; // Exit loop if no more entries
 
-//         try directories.append(entry.?.name); // Append the directory name
+//         try files.append(entry.?.name); // Append the directory name
 //     }
 
 //     // Print each directory in the list
-//     for (directories.items) |item| {
+//     for (files.items) |item| {
 //         std.debug.print("\u{2502} \x1b[32m{s}\x1b[0m\u{2502}\n", .{item});
 //     }
 // }
 
 // Version 2
 const std = @import("std");
-const ArrayList = std.ArrayList;
-const Allocator = std.heap.page_allocator;
-const green = "\x1b[32m";
-const reset = "\x1b[0m";
 
 fn max(a: usize, b: usize) usize {
     if (a > b) {
@@ -42,8 +38,10 @@ fn max(a: usize, b: usize) usize {
 }
 
 pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
     const cwd = std.fs.cwd(); // Get the current working directory
-    var directories = ArrayList([]const u8).init(Allocator);
+    var files = std.ArrayList([]const u8).init(allocator);
 
     // Open the current directory explicitly with "."
     var dir = try cwd.openDir(".", .{ .iterate = true });
@@ -56,22 +54,22 @@ pub fn main() !void {
 
     // Collect directory names
     while (try iterator.next()) |entry| {
-        try directories.append(entry.name); // Append the directory name
+        try files.append(entry.name); // Append the directory name
     }
 
     // Find the maximum length of the directory names to properly align the table
     var max_len: usize = 0;
-    for (directories.items) |item| {
+    for (files.items) |item| {
         max_len = max(max_len, item.len);
     }
 
     // Print each directory name
-    for (directories.items) |item| {
+    for (files.items) |item| {
         const spaces_needed = max_len - item.len;
 
         // Dynamically allocate memory for spaces
-        var spaces = try Allocator.alloc(u8, spaces_needed);
-        defer Allocator.free(spaces);
+        var spaces = try allocator.alloc(u8, spaces_needed);
+        defer allocator.free(spaces);
 
         // Fill the spaces array with ' ' (spaces_needed times)
         for (0..spaces_needed) |i| {
