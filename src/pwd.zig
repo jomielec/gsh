ddae650a0565d359af0f7cbffc86bd3df4fe8fcb
Micah Hall <hallmicah10@gmail.com>
2024-11-30 13:38:39 -0500

    Add pwd.zig

---
+++ src/pwd.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const file = try std.fs.cwd().createFile(
+        "junk_file.txt",
+        .{ .read = true },
+    );
+    defer file.close();
+
+    const bytes_written = try file.writeAll("Hello File!");
+    _ = bytes_written;
+
+    var buffer: [100]u8 = undefined;
+    try file.seekTo(0);
+    const bytes_read = try file.readAll(&buffer);
+
+    try expect(eql(u8, buffer[0..bytes_read], "Hello File!"));
+}
